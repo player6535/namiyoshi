a6ca2a854b420bc21e06af107cce0265023908db
player6535 <60130509+player6535@users.noreply.github.com>
2023-07-31 23:56:43 +0900

    Add files via upload
    add namiyoshi!

---
+++ ナミヨシ塗装店flocss/assets/js/script.js
@@ -0,0 +1,7 @@
+$(function () {
+    // ハンバーガーメニュー
+    $(".js-hamburger,.js-drawer").click(function () {
+      $(".js-hamburger").toggleClass("is-active");
+      $(".js-drawer").fadeToggle();
+    });
+  });
