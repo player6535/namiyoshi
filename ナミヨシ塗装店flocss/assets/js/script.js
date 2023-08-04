f9104024eff0beaf20d5ef42627f0d9c358bb814
player6535 <60130509+player6535@users.noreply.github.com>
2023-08-04 15:10:14 +0900

    Add files via upload

--- ナミヨシ塗装店flocss/assets/js/script.js
+++ ナミヨシ塗装店flocss/assets/js/script.js
@@ -1,7 +1,27 @@
 $(function () {
-    // ハンバーガーメニュー
-    $(".js-hamburger,.js-drawer").click(function () {
-      $(".js-hamburger").toggleClass("is-active");
-      $(".js-drawer").fadeToggle();
-    });
+  // ハンバーガーメニュー
+  $(".js-hamburger,.js-drawer").click(function () {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-hamburger__title").toggleClass("is-active");
+    $(".js-drawer").fadeToggle();
   });
+});
+// 変数pagetopの宣言
+var pagetop = $('.p-pageTop');
+// スクロールイベント
+$(window).on('scroll', function () {//スクロールしたとき、
+  if ($(this).scrollTop() > 700) { //スクロール量が500px以上なら、
+    pagetop.addClass('active');    //activeクラスを付与し、
+  } else {                         //500px未満なら、
+    pagetop.removeClass('active'); //activeクラスを外します。
+  }
+});
+// ページトップへ戻るボタンをクリックしたとき
+pagetop.on('click', function () {
+  $('html, body').animate(
+    { scrollTop: 0 },
+    400,
+    'swing',
+  );
+  return false;
+});
